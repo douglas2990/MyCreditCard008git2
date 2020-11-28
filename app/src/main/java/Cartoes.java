55179ac39f463d15295056434736d11f7820096e
Douglas <douglas2990@hotmail.com>
2020-11-27 22:00:32 -0300

    atualizando

---
+++ app/src/main/java/Cartoes.java
@@ -0,0 +1,33 @@
+public class Cartoes {
+
+    String item;
+    String data;
+    double valor;
+
+    public Cartoes() {
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
---
+++ app/src/main/java/com/example/mycreditcard008/HiperCardActivity.java
@@ -0,0 +1,14 @@
+package com.example.mycreditcard008;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HiperCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiper_card);
+    }
+}
